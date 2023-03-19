6d9fe70e28730a235cc387f75d75073d0d15bf4a
tanvir160 <tanvir767676@gmail.com>
2023-03-20 00:07:39 +0600

    first commit

--- pages/Chainsaw.js
+++ pages/Chainsaw.js
@@ -1,16 +1,16 @@
-
+import style from "../styles/Chainsaw.module.css";
 const Chainsaw = () => {
-    return (
-       <div>
-        <head>
-            <title>best chainsaw</title>
-        </head>
-        <section>
-      <a href="https://www.facebook.com/tanvir7676/"> tanvir</a>
-        </section>
-       </div>
-      
-    );
+  return (
+    <div>
+      <head>
+        <title>best chainsaw</title>
+      </head>
+      <section className="bg-danger">
+        <a className={style.s} href="https://www.facebook.com/tanvir7676/"> tanvir</a>
+        <h1 className={style.d}>amar sonar bangla ami tumar</h1>
+      </section>
+    </div>
+  );
 };
 
 export default Chainsaw;
