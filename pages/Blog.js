aaddb36080a383371f58592482a6b91a9883e1db
tanvir160 <tanvir767676@gmail.com>
2023-03-19 22:03:52 +0600

    first commit

--- pages/Blog.js
+++ pages/Blog.js
@@ -5,10 +5,48 @@ import Image from "next/image";
 const Blog = () => {
   return (
     <div className="mt-5">
-      <div class="container text-center ">
-        <div class="row gap-5">
+      <div className="container text-center ">
+        <div className="row gap-5">
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
+            <Link href={`/Chack`}>
+              <div className="card  " style={{ width: 300 }}>
+                <Image src="/tan.png" width="300" height="200"></Image>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </Link>
+          </div>
+          {/* <>end card</> */}
+          {/* <>start card</> */}
+          <div className="col-sm-12 col-lg-3 ">
+            <Link href={`/Chainsaw`}>
+              <div className="card " style={{ width: 300 }}>
+                <Image src="/tan.png" width="300" height="200"></Image>
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="" className="btn btn-primary">
+                    Go somewhere
+                  </a>
+                </div>
+              </div>
+            </Link>
+          </div>
+          {/* <>end card</> */}
+          {/* <>start card</> */}
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -27,7 +65,7 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -46,7 +84,7 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -65,7 +103,7 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -84,7 +122,7 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -103,7 +141,7 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
+          <div className="col-sm-12 col-lg-3 ">
             <Link href="">
               <div className="card " style={{ width: 300 }}>
                 <Image src="/tan.png" width="300" height="200"></Image>
@@ -122,49 +160,12 @@ const Blog = () => {
           </div>
           {/* <>end card</> */}
           {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
-            <Link href="">
-              <div className="card " style={{ width: 300 }}>
-                <Image src="/tan.png" width="300" height="200"></Image>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </Link>
-          </div>
-          {/* <>end card</> */}
-          {/* <>start card</> */}
-          <div class="col-sm-12 col-lg-3 ">
-            <Link href="">
-              <div className="card " style={{ width: 300 }}>
-                <Image src="/tan.png" width="300" height="200"></Image>
-                <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </p>
-                  <a href="" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </Link>
-          </div>
-          {/* <>end card</> */}
-          {/* <>start card</> */}
-          <div class="col">Column</div>
+          <div className="col">Column</div>
           {/* <>end card</> */}
         </div>
       </div>
     </div>
+   
   );
 };
 
---
+++ pages/Chainsaw.js
@@ -0,0 +1,16 @@
+
+const Chainsaw = () => {
+    return (
+       <div>
+        <head>
+            <title>best chainsaw</title>
+        </head>
+        <section>
+      <a href="http://localhost:3000/Chack"> tanvir</a>
+        </section>
+       </div>
+      
+    );
+};
+
+export default Chainsaw;
